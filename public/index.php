4c34c3bff4e76663651155684dbfa90c841c8cb6
joao pedro Vijales Schneider <vijales2000@gmail.com>
2025-05-18 10:43:06 -0300

    Atualiza arquivos do projeto

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
+declare(strict_types=1);
+
+// Linhas temporárias para exibição de erro - remova ou comente em produção
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+
+
 /**
  * Ponto de entrada principal da aplicação (Front Controller)
  */
-
-declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
