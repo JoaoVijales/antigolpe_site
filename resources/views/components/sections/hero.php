6e6f123817f2d7c5c6b238270ae6b947f9dd0252
joao pedro Vijales Schneider <vijales2000@gmail.com>
2025-05-18 13:16:58 -0300

    Corrigir uso de View::asset e adicionar cta_text ao Hero

--- resources/views/components/sections/hero.php
+++ resources/views/components/sections/hero.php
@@ -6,10 +6,10 @@
     <h1><?= $heroData['title'] ?></h1>
     <p class="subtitle"><?= $heroData['subtitle'] ?></p>
     <button class="cta-button" data-action="scroll-to-form">
-      <?= $heroData['cta_text'] ?>
+      <?= $heroData['cta_text'] ?? 'Saiba Mais' // Usar Saiba Mais como fallback ?>
     </button>
   </div>
   <div class="hero-image">
-    <img src="<?= $this->asset($heroData['image']) ?>" alt="<?= $heroData['image_alt'] ?>">
+    <img src="<?= App\Utils\View::asset($heroData['image']) ?>" alt="<?= $heroData['image_alt'] ?? 'Imagem Hero' ?>">
   </div>
 </section>
