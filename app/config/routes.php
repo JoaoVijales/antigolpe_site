08677bd03b0d04c6ce94a88b79f3a59602c607ce
joao pedro Vijales Schneider <vijales2000@gmail.com>
2025-05-21 20:08:42 -0300

    Refactor(dashboard): Rename dashboard controller method to 'dashboard'

--- app/config/routes.php
+++ app/config/routes.php
@@ -11,7 +11,7 @@ return function (RouteCollector $r) {
     // Rotas Web
     $r->addGroup('', function (RouteCollector $r) {
         $r->addRoute('GET', '/', [App\Controllers\HomeController::class, 'index']);
-        $r->addRoute('GET', '/dashboard', [App\Controllers\DashboardController::class, 'securityDashboard']);
+        $r->addRoute('GET', '/dashboard', [App\Controllers\DashboardController::class, 'dashboard']);
         $r->addRoute('GET', '/404', [App\Controllers\HomeController::class, 'notFound']);
         $r->addRoute('GET', '/dashboard/verify-whatsapp', [App\Controllers\DashboardController::class, 'verifyWhatsapp']);
     });
